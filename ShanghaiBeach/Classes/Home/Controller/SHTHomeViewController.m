db0b026ced5f693a54ad7e9439aca5da22629b7e
Harvey Yang <harveyMac@localhost>
2016-04-21 10:38:42 +0800

    格式化代码

--- ShanghaiBeach/Classes/Home/Controller/SHTHomeViewController.m
+++ ShanghaiBeach/Classes/Home/Controller/SHTHomeViewController.m
@@ -21,127 +21,127 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
-    [self setupNav];
-    
-    [self setupTitlesView];
-    
-    [self setupContentView];
-
-   
+	[super viewDidLoad];
+	
+	[self setupNav];
+	
+	[self setupTitlesView];
+	
+	[self setupContentView];
+	
+	
 }
 
 -(void)setupContentView
 {
-    //不要自动调整inset
-    self.automaticallyAdjustsScrollViewInsets = NO;
-    
-    UIScrollView *contentView = [[UIScrollView alloc] init];
-    
-    contentView.frame = self.view.bounds;
-    
-    //以下方案导致scrollview不能有穿透效果
+	//不要自动调整inset
+	self.automaticallyAdjustsScrollViewInsets = NO;
+	
+	UIScrollView *contentView = [[UIScrollView alloc] init];
+	
+	contentView.frame = self.view.bounds;
+	
+	//以下方案导致scrollview不能有穿透效果
 //    contentView.width = self.view.width;
 //    contentView.y = 99;
 //    contentView.height = self.view.height - contentView.y - self.tabBarController.tabBar.height;
-    
-    CGFloat top = CGRectGetMaxY(self.titlesView.frame);
-    CGFloat buttom = self.tabBarController.tabBar.height;
-    contentView.contentInset = UIEdgeInsetsMake(top, 0, buttom, 0);
-    contentView.backgroundColor = [UIColor yellowColor];
-    [self.view insertSubview:contentView atIndex:0];
-    //[self.view addSubview:contentView];
+
+	CGFloat top = CGRectGetMaxY(self.titlesView.frame);
+	CGFloat buttom = self.tabBarController.tabBar.height;
+	contentView.contentInset = UIEdgeInsetsMake(top, 0, buttom, 0);
+	contentView.backgroundColor = [UIColor yellowColor];
+	[self.view insertSubview:contentView atIndex:0];
+	//[self.view addSubview:contentView];
 }
 
 -(void)setupTitlesView
 {
-    //标签栏整体
-    UIView *titlesView = [[UIView alloc] init];
-    //颜色设置透明度选择方案
-    //titlesView.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
-    //titlesView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.5];
-    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
-    
-    titlesView.width = self.view.width;
-    titlesView.height = 35;
-    titlesView.y = 64;
-    [self.view addSubview:titlesView];
-    self.titlesView = titlesView;
-    
-    //底部的红色指示器
-    UIView *indicatorView = [[UIView alloc] init];
-    indicatorView.height = 2;
-    indicatorView.y = titlesView.height - indicatorView.height;
-    indicatorView.backgroundColor = [UIColor redColor];
-    
-    [titlesView addSubview:indicatorView];
-    self.indicatorView = indicatorView;
-    
-    //内部的子标签
-    NSArray *titles = @[@"全部", @"视频", @"声音", @"图片", @"段子"];
-    CGFloat width = self.view.width / titles.count;
-    CGFloat height = titlesView.height;
-    
-    for(NSInteger i = 0; i < titles.count; i++){
-        UIButton *button = [[UIButton alloc] init];
-        button.width = width;
-        button.height = height;
-        button.x = i * width;
-        [button setTitle:titles[i] forState:UIControlStateNormal];
-        //[button layoutIfNeeded]; //强制布局（强制更新子控件的frame）
-        [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
-        [button setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
-        button.titleLabel.font = [UIFont systemFontOfSize:14];
-        [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
-        
-        [titlesView addSubview:button];
-        
-        //默认点击了第一个按钮
-        if(i == 0){
-            button.enabled = NO;
-            self.selectedButton = button;
-            
-            //让按钮内部的labe感觉文字内容来计算尺寸
-            //[button.titleLabel sizeToFit];
-            //计算宽度第二种方案
-            self.indicatorView.width = [titles[i] sizeWithAttributes:@{NSFontAttributeName:button.titleLabel.font}].width;
-            //self.indicatorView.width = button.titleLabel.width;
-            self.indicatorView.centerX = button.centerX;
-        }
-    }
-    
+	//标签栏整体
+	UIView *titlesView = [[UIView alloc] init];
+	//颜色设置透明度选择方案
+	//titlesView.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
+	//titlesView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+	titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+	
+	titlesView.width = self.view.width;
+	titlesView.height = 35;
+	titlesView.y = 64;
+	[self.view addSubview:titlesView];
+	self.titlesView = titlesView;
+	
+	//底部的红色指示器
+	UIView *indicatorView = [[UIView alloc] init];
+	indicatorView.height = 2;
+	indicatorView.y = titlesView.height - indicatorView.height;
+	indicatorView.backgroundColor = [UIColor redColor];
+	
+	[titlesView addSubview:indicatorView];
+	self.indicatorView = indicatorView;
+	
+	//内部的子标签
+	NSArray *titles = @[@"全部", @"视频", @"声音", @"图片", @"段子"];
+	CGFloat width = self.view.width / titles.count;
+	CGFloat height = titlesView.height;
+	
+	for(NSInteger i = 0; i < titles.count; i++) {
+		UIButton *button = [[UIButton alloc] init];
+		button.width = width;
+		button.height = height;
+		button.x = i * width;
+		[button setTitle:titles[i] forState:UIControlStateNormal];
+		//[button layoutIfNeeded]; //强制布局（强制更新子控件的frame）
+		[button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+		[button setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
+		button.titleLabel.font = [UIFont systemFontOfSize:14];
+		[button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+		
+		[titlesView addSubview:button];
+		
+		//默认点击了第一个按钮
+		if(i == 0) {
+			button.enabled = NO;
+			self.selectedButton = button;
+			
+			//让按钮内部的labe感觉文字内容来计算尺寸
+			//[button.titleLabel sizeToFit];
+			//计算宽度第二种方案
+			self.indicatorView.width = [titles[i] sizeWithAttributes:@{NSFontAttributeName:button.titleLabel.font}].width;
+			//self.indicatorView.width = button.titleLabel.width;
+			self.indicatorView.centerX = button.centerX;
+		}
+	}
+	
 }
 
 -(void)titleClick:(UIButton *)button
 {
-    //处理选中按钮
-    self.selectedButton.enabled = YES;
-    button.enabled = NO;
-    self.selectedButton = button;
-    //动画切换
-    [UIView animateWithDuration:0.25 animations:^{
-        self.indicatorView.width = button.titleLabel.width;
-        self.indicatorView.centerX = button.centerX;
-    }];
+	//处理选中按钮
+	self.selectedButton.enabled = YES;
+	button.enabled = NO;
+	self.selectedButton = button;
+	//动画切换
+	[UIView animateWithDuration:0.25 animations:^{
+	         self.indicatorView.width = button.titleLabel.width;
+	         self.indicatorView.centerX = button.centerX;
+	 }];
 }
 
 -(void)setupNav
 {
-    // 设置导航栏标题
-    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
-    
-    // 设置导航栏左边的按钮
-    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImage:@"MainTagSubIcon" highImage:@"MainTagSubIconClick" target:self action:@selector(tagClick)];
-    
-    // 设置背景色
-    self.view.backgroundColor = XMGGlobalBg;
+	// 设置导航栏标题
+	self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
+	
+	// 设置导航栏左边的按钮
+	self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImage:@"MainTagSubIcon" highImage:@"MainTagSubIconClick" target:self action:@selector(tagClick)];
+	
+	// 设置背景色
+	self.view.backgroundColor = XMGGlobalBg;
 }
 
 - (void)tagClick
 {
-    SHTRecommendTagsViewController *tags = [[SHTRecommendTagsViewController alloc] init];
-    [self.navigationController pushViewController:tags animated:YES];
+	SHTRecommendTagsViewController *tags = [[SHTRecommendTagsViewController alloc] init];
+	[self.navigationController pushViewController:tags animated:YES];
 }
 
 @end
--- ShanghaiBeach/Classes/Home/Controller/SHTRecommendTagsViewController.m
+++ ShanghaiBeach/Classes/Home/Controller/SHTRecommendTagsViewController.m
@@ -29,80 +29,80 @@ static NSString * const SHTTagsId = @"tag";
 @implementation SHTRecommendTagsViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    self.title = @"推荐标签";
-    
-    [self setupTableView];
-    
-    [self loadTags];
-    
-    
+	[super viewDidLoad];
+	
+	self.title = @"推荐标签";
+	
+	[self setupTableView];
+	
+	[self loadTags];
+	
+	
 }
 
 -(void)setupTableView
 {
-    [self.tableView registerNib:[UINib nibWithNibName:NSStringFromClass([SHTRecommendTagCell class]) bundle:nil] forCellReuseIdentifier:SHTTagsId];
-    self.tableView.rowHeight = 70;
-    self.tableView.separatorStyle = UITableViewCellEditingStyleNone;
-    self.tableView.backgroundColor = XMGGlobalBg;
+	[self.tableView registerNib:[UINib nibWithNibName:NSStringFromClass([SHTRecommendTagCell class]) bundle:nil] forCellReuseIdentifier:SHTTagsId];
+	self.tableView.rowHeight = 70;
+	self.tableView.separatorStyle = UITableViewCellEditingStyleNone;
+	self.tableView.backgroundColor = XMGGlobalBg;
 }
 
 - (void)loadTags
 {
-    [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeBlack];
-    
-    // 请求参数
-    NSMutableDictionary *params = [NSMutableDictionary dictionary];
-    params[@"a"] = @"tag_recommend";
-    params[@"action"] = @"sub";
-    params[@"c"] = @"topic";
-    
-    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params progress:^(NSProgress * _Nonnull downloadProgress) {
-        
-    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        NSLog(@"%@", responseObject);
-        self.tags = [SHTRecommendTag mj_objectArrayWithKeyValuesArray:responseObject];
-        [self.tableView reloadData];
-        [SVProgressHUD dismiss];
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        
-        [SVProgressHUD showErrorWithStatus:@"加载标签数据失败!"];
-    }];
-    
-    // 发送请求
-//    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
-//        //self.tags = [XMGRecommendTag objectArrayWithKeyValuesArray:responseObject];
-//        //[self.tableView reloadData];
-//        NSLog(@"%@", responseObject);
-//        
-//        [SVProgressHUD dismiss];
-//    } failure:^(NSURLSessionDataTask *task, NSError *error) {
-//        [SVProgressHUD showErrorWithStatus:@"加载标签数据失败!"];
-//    }];
+	[SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeBlack];
+	
+	// 请求参数
+	NSMutableDictionary *params = [NSMutableDictionary dictionary];
+	params[@"a"] = @"tag_recommend";
+	params[@"action"] = @"sub";
+	params[@"c"] = @"topic";
+	
+	[[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params progress:^(NSProgress * _Nonnull downloadProgress) {
+	
+	 } success:^(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObject) {
+	         NSLog(@"%@", responseObject);
+	         self.tags = [SHTRecommendTag mj_objectArrayWithKeyValuesArray:responseObject];
+	         [self.tableView reloadData];
+	         [SVProgressHUD dismiss];
+	 } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+	 
+	         [SVProgressHUD showErrorWithStatus:@"加载标签数据失败!"];
+	 }];
+	 
+	// 发送请求
+	//    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
+	//        //self.tags = [XMGRecommendTag objectArrayWithKeyValuesArray:responseObject];
+	//        //[self.tableView reloadData];
+	//        NSLog(@"%@", responseObject);
+	//
+	//        [SVProgressHUD dismiss];
+	//    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+	//        [SVProgressHUD showErrorWithStatus:@"加载标签数据失败!"];
+	//    }];
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    return self.tags.count;
+	return self.tags.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    SHTRecommendTagCell *cell = [tableView dequeueReusableCellWithIdentifier:SHTTagsId];
-    
-    // Configure the cell...
-    cell.recommendTag = self.tags[indexPath.row];
-    
-    return cell;
+
+	SHTRecommendTagCell *cell = [tableView dequeueReusableCellWithIdentifier:SHTTagsId];
+	
+	// Configure the cell...
+	cell.recommendTag = self.tags[indexPath.row];
+	
+	return cell;
 }
 
 
